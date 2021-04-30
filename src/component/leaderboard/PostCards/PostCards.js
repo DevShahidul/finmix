6b77ac5ffe59e3fbc2795b35f39be50bf876ced4
Kamal Hossan <45435313+kamalhossan@users.noreply.github.com>
2021-04-30 15:44:48 +0600

    changes

--- src/component/leaderboard/PostCards/PostCards.js
+++ src/component/leaderboard/PostCards/PostCards.js
@@ -11,7 +11,7 @@ const PostsCards = props => {
     <div className="posts-cards">
         <div className="post-status">
           <div className="profile-picure">
-              <img src="https://scontent.fdac40-1.fna.fbcdn.net/v/t1.6435-1/cp0/p40x40/100770718_2061640880649037_8748155934439636992_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=7206a8&_nc_eui2=AeECQt-jr4cOaVQkhQZv6I0xKp4NdlkxnKAqng12WTGcoLu8RuYan1pnsUDQ31RyNVhAnEXhrHdN2CPlFXLREYDS&_nc_ohc=SKCTyNgZksYAX_Q4RBo&_nc_ht=scontent.fdac40-1.fna&tp=27&oh=7e8634fbb231b565f3a07be70aaca979&oe=60ADED28" alt="kamal Hossan"/>
+              <img src="https://hist.cms.arts.ubc.ca/wp-content/uploads/sites/23/2019/06/IMG_1422-2.jpg" alt="kamal Hossan"/>
           </div>
           <div className="text-area">
             <Input.TextArea placeholder="Whats on your mind"/>
